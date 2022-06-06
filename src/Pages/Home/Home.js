896abc6eb01f3f41ae609c17ec73f440b90ab863
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-06 12:04:24 +0600

    init project

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            This is home 
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxIBFmxwUmKSaerOWE0Jqje4fc4emYFNI",
+  authDomain: "pharmecy-management-firebase.firebaseapp.com",
+  projectId: "pharmecy-management-firebase",
+  storageBucket: "pharmecy-management-firebase.appspot.com",
+  messagingSenderId: "533090694614",
+  appId: "1:533090694614:web:8a086989e6233ce2506077"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
